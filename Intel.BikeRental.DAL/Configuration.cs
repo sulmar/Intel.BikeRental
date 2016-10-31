eeca240e8851d15619ccaf21b097ece609657f51
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-10-31 08:19:36 +0100

    Dodano klasę konfiguracyjną oraz przykład interceptora oraz strategii

---
+++ Intel.BikeRental.DAL/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using Intel.BikeRental.DAL.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intel.BikeRental.DAL
+{
+    public class Configuration : DbConfiguration
+    {
+        public Configuration()
+        {
+            this.AddInterceptor(new NLogCommandInterceptor());
+
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy(10, TimeSpan.FromSeconds(5)));
+
+        }
+    }
+}
